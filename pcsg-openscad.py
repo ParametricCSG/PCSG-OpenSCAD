0066d3b31a6b53f1bbb779e7c892e53f389758c4
Steve Kelly <kd2cca@gmail.com>
2013-07-12 23:19:35 -0500

    add support for translation and rotation operations

--- pcsg-openscad.py
+++ pcsg-openscad.py
@@ -26,32 +26,45 @@ parser.add_argument('--version', action='version', version="%(prog)s 0.0.1-dev")
 #Always output help by default
 if len(sys.argv) == 1:
     parser.print_help()
-    sys.exit(0)
+    sys.exit(0) # exit after help display
+
 args = parser.parse_args()
 
 class OpenSCADEngine:
     def __init__(self):
         self.level = 0
         self.output = ""
-        self.operations = ["union", "difference", "intersection", "hull"]
+        self.operations = ["union", "difference", "intersection", "hull", "translate", "rotate"]
         self.elements = ["cube", "cylinder", "sphere", "cone"]
-        
+
     def parseJSON(self, data):
         if data['category']:
             if data['category'] == "operation":
                 if data['name'] in self.operations:
-                    a = data['name'] + "(){\n"                  
+                    a = ""
+                    if 'color' in data and len(data['color']) >= 3:
+                        a = "color(" + str(data['color']) + ")"
+                    if data['name'] == "translate":
+                        a += "translate(" + str(data['location']) + "){"
+                    if data['name'] == "rotate":
+                        a += self.processRotation(data) + "{"
+                    else:
+                        a = data['name'] + "(){\n"
                     self.output += " " * self.level * args.indent + a
                     for types in data['elements']:
                         self.level += 1
                         self.parseJSON(types)
                         self.level -= 1
                     self.output += " " * self.level * args.indent + "}\n"
+                else:
+                    a = "echo("+data['name']+");"
+                    self.output += a
+                    self.parseJSON(data['construction'])
             if data['category'] == "element":
+                if 'color' in data and len(data['color']) >= 3:
+                    self.output += "color(" + str(data['color']) + ")"
                 if data['name'] in self.elements:
                     a = ""
-                    if len(data['color']) >= 3:
-                        a = "color(" + str(data['color']) + ")"
                     a += "translate(" + str(data['location']) + ")"
                     a += "rotate(a=" + str(data['rotation']['angle']) + ", v=" + \
                          str(data['rotation']['axis']) + ")"
@@ -60,11 +73,12 @@ class OpenSCADEngine:
                     if data['name'] == "sphere":
                         a += self.makeSphere(data)
                     if data['name'] == "cylinder":
-                        a += self.makeCylinder(data)     
-                    self.output += " " * self.level + a
+                        a += self.makeCylinder(data)
+                    self.output += "    " * self.level + a
                 else:
-                    sys.stderr.write("ERROR: Unknown element name\n")
-				    
+                    print "Found top level element " + data['name']
+                    self.parseJSON(data['construction'])
+
     def makeCube(self, data):
         """Take a python dictionary and make an OpenSCAD compatible Cube string"""
         a = ""
@@ -76,7 +90,7 @@ class OpenSCADEngine:
         a += "translate(" + str(centering) + ")"
         a += "cube(size=" + str(data['size']) + ");\n"
         return a
-        
+
     def makeSphere(self, data):
         """Take a python dictionary and make an OpenSCAD compatible Sphere string"""
         a = ""
@@ -86,9 +100,9 @@ class OpenSCADEngine:
             if not val:
                 centering[idx] += data['radius']
         a += "translate(" + str(centering) + ")"
-        a += "sphere(r=" + str(data['radius']) + ");\n"
+        a += "sphere(r=" + str(data['radius']) + ", $fn=" + str(data['radius']) + ");\n"
         return a
-        
+
     def makeCylinder(self, data):
         """Take a python dictionary and make an OpenSCAD compatible Cube string"""
         a = ""
@@ -100,9 +114,14 @@ class OpenSCADEngine:
             elif val and idx == 2:
                 centering[idx] = -data['height']/2
         a += "translate(" + str(centering) + ")"
-        a += "cylinder(r=" + str(data['radius']) + ", h=" + str(data['height']) + ");\n"
+        a += "cylinder(r=" + str(data['radius']) + ", h=" + str(data['height']) \
+              + ", $fn=" + str((data['radius'])*20) + ");\n"
         return a
-            
+
+    def processRotation(self, data):
+        return "rotate(a=" + str(data['rotation']['angle']) + ", v=" + \
+                         str(data['rotation']['axis']) + ")"
+
 j = json.loads(args.input.read())
 c = OpenSCADEngine()
 c.parseJSON(j)
