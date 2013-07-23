d90c5c246695956a565372d2cbe26e81f885386e
Steve Kelly <kd2cca@gmail.com>
2013-07-23 13:36:17 -0500

    update install script

--- install.sh
+++ install.sh
@@ -5,8 +5,14 @@ SCIPT_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
 echo "Installing Python modules..."
 python3.3 $SCRIPT_PATH/setup.py install
 
-echo "Installing pcsg-openscad"
+echo "Running tests"
+cd tests
+python3.3 -m unittest -v
+cd ..
+
+echo "Installing textcad in /usr/local/bin/"
 cp $SCRIPT_PATH/textcad_engine.py /usr/local/bin/textcad
 chmod +x /usr/local/bin/textcad
 
 echo "Finished!"
+
